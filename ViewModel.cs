4255b793c4eaa28817633f986c7f99afd5d8d030
AlexandR Mel_hoff <148494057+Alex-Melhoff@users.noreply.github.com>
2025-04-04 19:19:23 +0300

    Add files via upload
    ViewModel and Model classes were created.

---
+++ ViewModel.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Reflection.Metadata;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EducateApp
+{
+    interface IName
+    {
+        string GetText(IName iname);
+        void SetText(IName iname, string text);
+    }
+    class ViewModel : INotifyPropertyChanged
+    {
+        public ViewModel() 
+        { 
+        
+        }
+
+
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        public void OnPropertyChanged([CallerMemberName] string prop = "")
+        {
+            if (PropertyChanged != null)
+                PropertyChanged(this, new PropertyChangedEventArgs(prop));
+        }
+    }
+}
